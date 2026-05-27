3ed96142756556e882d201aa8af7fd1399661b71
ruijpedro <rjpedro@gmail.com>
2026-05-27 11:15:45 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='eixo-oeste-amv-rjp-v1';
-const ASSETS=['./','./index.html','./src/styles.css','./src/app.js','./manifest.webmanifest','./assets/logo_rjp_amv.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
