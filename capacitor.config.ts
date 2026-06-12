f2df44f3d4b47955ac1c4949e53bd1be654902e4
ruijpedro <rjpedro@gmail.com>
2026-06-12 15:41:49 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'rjp.eixooeste.amv',
+  appName: 'Eixo Oeste AMV',
+  webDir: 'www'
+};
+
+export default config;
